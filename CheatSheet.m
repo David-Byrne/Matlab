71e840e5da2e0c1181b5453557b79db63515609a
David Byrne <David.dbyrne@gmail.com>
2016-01-10 15:37:07 +0000

    Centers and grounds the pyramid

---
+++ CheatSheet.m
@@ -0,0 +1,27 @@
+%This is how you comment
+
+% It's vector based so variables are vectors (arrays) by default
+a = 3;
+b = [1 2 3 4 5];
+% the ; stops the line's results being printed to the colsole
+
+mat = zeros(a, a);
+% fills mat with an axb matrix with each value 0
+
+
+if a == 4
+    %do something
+elseif a == 5
+    %do something else
+else
+    %do a different thing
+end
+  
+
+for i = 1: 10
+    %do this each time
+end
+
+
+
+
--- Pyramid.m
+++ Pyramid.m
@@ -5,13 +5,15 @@ for i = 1: n
         val1 = i;
         val2 = j;
         if (val1 > n/2)
-            val1 = n-val1;
+            val1 = n+1-val1;
+            % n+1 to centre correctly around the middle
         end
         if (val2 > n/2)
-            val2 = n-val2;
+            val2 = n+1-val2;
         end
-        pyr(i,j) = (4/3)*(min(val1,val2));
+        pyr(i,j) = 4/3*(min(val1,val2)-1);
         %max height 2/3 of length of base, 2*(2/3)=4/3
+        % -1 so the lowest values are 0 as opposed to 1 (first index)
     end
 end
 surf(pyr) %display surface of pyramid
